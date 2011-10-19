1347671351c72552561b7635bc5d04ab55c3c6e7
Steven Heyninck <steven.heyninck@cronos.be>
2011-10-19 13:06:53 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PipRace::Application.config.secret_token = '40115768929acc8260f39d7628612b15ec9d51a3fc91f1e9375d2288ed7a4b5a7af52a3e24f554925d904091ee6136b735a0493654a26c7e73e08885d90eee18'
